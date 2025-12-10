0d938db706e4907ce6f9c43c1b314919979ece9d
Gabriela Araujo <gabyara237@gmail.com>
2025-12-10 15:45:16 -0800

    feat: add new recipe form and create recipe route

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,5 +3,29 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 const Recipe = require('../models/recipe.js');
+const Ingredient = require('../models/ingredient.js');
 
+router.get('/', async(req,res)=> {
+    res.render('recipes/index.ejs');
+});
+
+
+router.get('/new', async(req,res) =>{
+    res.render('recipes/new.ejs');
+})
+
+router.post('/',async(req,res)=>{
+    try{
+        const owner = req.session.user._id;
+        req.body.owner = owner;
+        await Recipe.create(req.body)
+
+        res.redirect("/recipes");
+
+    }catch (error){
+        console.log(error);
+        res.redirect("/")
+    }
+
+})
 module.exports = router;
